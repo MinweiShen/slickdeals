dda5957437823a0c7b7be4b5ef174c97489039f9
Minwei Shen <minwei@conviva.com>
2016-07-07 14:50:51 -0700

    add search functionality

--- README.md
+++ README.md
@@ -3,14 +3,25 @@
 
 A command line tool for [Slickdeals](http://slickdeals.net).
 
+## Dependency
+1. requests
+2. beautifulsoup
+
 ## Usage
 python sclickdeals.py [option]
 
 ### Options
-1. -f --free: show items free or ship free
-2. -F --FREE: show items which are free
-3. -n NUM: limit the number of results
+1. ``-f --free``: show items free or ship free on frontpage
+2. ``-F --FREE``: show items which are free on frontpage
+3. ``-n NUM``: limit the number of results
+4. ``-s 'search string'``: search item, quotation marks are required especially when your search string contains space
 
 
 ### Example
-1. ``python slickdeals.py -F -n 10`` : show first 10 free items
+1. ``python slickdeals.py -F -n 10`` : show first 10 free items on frontpage
+2. ``python sclickdeals.py -s 'amazon xbox'`: search amazon xbox deals
+3. ``python slickdeals.py -s 'amazon' -f -n 10``: search free amazon deals, show 10 results
+
+
+
+
--- utils.py
+++ utils.py
@@ -16,45 +16,92 @@ def parse_command():
                       help='show free deals only')
     parser.add_option('-n', action='store', type='int',
                       dest='num', help='limit the num of results')
+    parser.add_option('-s', '--search', action='store',
+                      type='string', dest='search_for',
+                      help='search for string')
 
     return parser.parse_args()
 
 
-def crawl_deals():
-    options, args = parse_command()
+def crawl_front_page(options, args):
     show_free = options.show_free
     show_free_price = options.show_free_price
     r = requests.get(SLICKDEALS)
     soup = BeautifulSoup(r.text, 'html.parser')
 
-    fpdeals = soup.find_all('div', {'class': 'frontpage'})
+    removeHidden = soup.find_all('div', {'class': 'removeHidden'})
+    fpdeals = []
+    for r in removeHidden:
+        fpdeals += r.find_all('div', {'class': 'frontpage'})
     deals = []
+
     for d in fpdeals:
         a = d.find('a', {'class': 'itemTitle'})
         link = SLICKDEALS + a['href']
         price_line = d.find('div', {'class': 'priceLine'})
         title = price_line['title']
         price_div = price_line.find('div', {'class': 'itemPrice'})
-        price = price_div and price_div.string.strip().lower() or 'None'
+        price = price_div and price_div.getText().strip() or 'None'
         shipinfo_div = price_line.find('div', {'class': 'priceInfo'})
-        shipinfo = shipinfo_div and shipinfo_div.string.strip().lower() or 'None'
+        shipinfo = shipinfo_div and shipinfo_div.getText().strip() or 'None'
         item = {
             'title': title,
             'link': link,
             'price': price,
-            'shipinfo': shipinfo
+            'info': shipinfo
         }
         if show_free_price:
-            if 'free' in price:
+            if 'free' in price.lower():
                 deals.append(item)
         elif show_free:
-            if 'free' in shipinfo or 'free' in price:
+            if 'free' in shipinfo.lower() or 'free' in price.lower():
                 deals.append(item)
         else:
             deals.append(item)
     if options.num:
         deals = deals[:options.num]
     return deals
+
+
+def crawl_search_page(options, args):
+    search_for = options.search_for.replace(' ', '+')
+    url = SLICKDEALS+'/newsearch.php?src = SearchBarV2&q='+search_for
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    results = soup.find('div', {'data-module-name': 'Search Results'})
+    results = results.find_all('div', {'class': 'resultRow'})
+    deals = []
+    for r in results:
+        a = r.find('a', {'class': 'dealTitle'})
+        title = a['title']
+        link = SLICKDEALS + a['href']
+        price = r.find('span', {'class': 'price'}).getText().strip()
+        rating = r.find('div', {'class': 'ratingNum'}).getText().strip()
+        expired = 'Expired' if 'expired' in r['class'] else ''
+        item = {
+            'title': title,
+            'link': link,
+            'price': price,
+            'info': rating + ' ' + expired
+        }
+        if options.show_free or options.show_free_price:
+            if 'free' in price.lower():
+                deals.append(item)
+        else:
+            deals.append(item)
+
+    if options.num:
+        deals = deals[:options.num]
+    return deals
+
+
+def crawl_deals():
+    options, args = parse_command()
+    if options.search_for:
+        return crawl_search_page(options, args)
+    else:
+        return crawl_front_page(options, args)
+
 
 
 
